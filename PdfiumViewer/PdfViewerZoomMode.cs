17b3b427441dd84d0f556b11d2c292f205baa3d0
Hossein Azadmanesh <mhazadmanesh2009@gmail.com>
2016-08-16 15:36:40 +0430

    Documentation for PdfViewerZoomMode as its values are not instantly obvious

--- PdfiumViewer/PdfViewerZoomMode.cs
+++ PdfiumViewer/PdfViewerZoomMode.cs
@@ -1,10 +1,20 @@
 ﻿namespace PdfiumViewer
 {
+    /// <summary>
+    /// Page zoom modes.
+    /// </summary>
     public enum PdfViewerZoomMode
     {
+        /// <summary>Fits the page height to the height of the control.</summary>
         FitHeight,
+
+        /// <summary>Fits the page width to the width of the control.</summary>
         FitWidth,
+
+        /// <summary>Fits the entire page in the control. This is also known as 'Whole Page' zoom.</summary>
         FitBest,
+
+        /// <summary>Maintains a fixed size for the page that doesn't change when the control is resized.</summary>
         FixedSize,
     }
 }
